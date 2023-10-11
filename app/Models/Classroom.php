d689395d1eba15f6f11a20444ffb855420ca84b3
bayu107 <bayuaja349@gmail.com>
2023-10-11 15:33:45 +0700

    update

--- app/Models/Classroom.php
+++ app/Models/Classroom.php
@@ -10,16 +10,4 @@ class Classroom extends Model
     use HasFactory;
 
     protected $guarded = [];
-
-    public function teacher(){
-        return $this->belongsTo(Teacher::class, 'teachers_id', 'id');
-    }
-
-    public function classroom(){
-        return $this->belongsTo(Classroom::class, 'classrooms_id', 'id');
-    }
-
-    public function periode(){
-        return $this->belongsTo(Periode::class, 'periode_id', 'id');
-    }
 }
